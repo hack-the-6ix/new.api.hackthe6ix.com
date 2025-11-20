e7ccfa60f04ef659eb36328a6e781548091ccd18
Daniel Pu <danielpu2007@gmail.com>
2025-11-19 20:52:30 -0500

    chore: change routing structure

--- src/api.ts
+++ src/api.ts
@@ -3,7 +3,8 @@ import seasonsRoute from "@/resources/seasons/seasons.routes";
 
 const api = new Hono();
 
-api.route("/seasons", seasonsRoute);
+api.route("/", seasonsRoute);
+
 api.get("/", (c) => c.json({ message: "Hello HT6 API!" }));
 
 export default api;
