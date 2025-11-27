86d7432a21becf667f2cfb05027aa5887537053a
Daniel Pu <danielpu2007@gmail.com>
2025-11-27 00:19:15 -0500

    feat(db): add new migration for cascading behaviour

--- src/db/schema/formResponse.ts
+++ src/db/schema/formResponse.ts
@@ -19,10 +19,12 @@ export const formResponse = pgTable(
       .default(sql`uuidv7()`),
     formId: uuid("formId").references(() => form.formId, {
       onDelete: "cascade",
+      onUpdate: "cascade",
     }),
     userId: uuid("userId").notNull(),
     seasonCode: char("seasonCode", { length: 3 }).references(
       () => season.seasonCode,
+      { onUpdate: "cascade" },
     ),
     responseJson: jsonb("responseJson").notNull(),
     isSubmitted: boolean("isSubmitted").default(false),
